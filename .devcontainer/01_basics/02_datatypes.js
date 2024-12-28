eb0cb414b8c35082e22e5839fb5182d90034d491
mschaware <144374028+mschaware@users.noreply.github.com>
2024-12-28 11:32:28 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+
+//alert (3+3) //we are using nodejs,not browser
+
+//code readability is must
+
+let name="hitest";
+let age=18;
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//undefined =>value not assigned
+//null=>standalone value
+//symbol=>uniques
+
+//object
+
+console.log(typeof undefined)
