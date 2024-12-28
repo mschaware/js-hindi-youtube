548e5cd8eac12b8bed7ce86688e2231bb29cad40
mschaware <144374028+mschaware@users.noreply.github.com>
2024-12-28 11:21:46 +0000

    Changes committed

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accEmail="mk"
+var accPass="12354"
+accCity="Jaipur"//not a good way
+let accState
+//accountId=2
+
+accEmail="sdjbx"
+accPass="xbs ik"
+accCity="manglore"
+
+console.log(accountId);
+
+/*
+
+*/
+
+console.table([accountId,accEmail,accPass,accCity,accState])
