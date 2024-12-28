8589607d271ed5fdf6f4cf1053cbb757f561fa28
mschaware <144374028+mschaware@users.noreply.github.com>
2024-12-28 11:44:00 +0000

    conversion of dt

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="hi"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc"=>NaN
+//true=>1;false=>zero
+
+
+//1=> true 0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
