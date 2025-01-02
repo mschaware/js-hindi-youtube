fe70b252be22d338dc171812d843ea7a875351b2
mschaware <144374028+mschaware@users.noreply.github.com>
2025-01-02 00:38:03 +0000

    destructor done

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -24,7 +24,30 @@ const obj2={3:"c",4:"d"};
 const obj3={...obj1,...obj2}//spread operator
 //console.log(obj3);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInsturctor:"hitesg"
+}
+
+//course.courseInsturctor
+const {courseInsturctor:ins}=course //destructor object
+
+console.log(ins);
+
+// {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {}
+]
