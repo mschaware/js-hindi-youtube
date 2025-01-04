01120d159c5b70c026e4f91e28de8244e5507356
Mayur Chaware <144374028+mschaware@users.noreply.github.com>
2025-01-04 02:51:19 +0000

    for

---
+++ .devcontainer/05_iterations/one.js
@@ -0,0 +1,18 @@
+//for samee
+for(let index=0;index<10;index++)
+{
+    const element=index;
+    console.log(element);
+}
+
+//break and conrinue
+for(let i=1;i<20;i++)
+{
+    if(i==5)
+    {
+        console.log("detected 5");
+        //break;
+        //continue;
+    }
+    console.log(`value of i is ${index}`);
+}
