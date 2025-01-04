667adcda263bce6a4f9d689d887bc9991830d094
Mayur Chaware <144374028+mschaware@users.noreply.github.com>
2025-01-04 02:19:04 +0000

    control flow

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,21 @@
+//if 
+const isuserloggedin=true;
+if(isuserloggedin){
+    //will run
+}
+//if(2=="2")console.log("executed for 2");
+//if(2=="2")console.log("executed for 2 ===");
+
+//<,>,>=,<=,!=,===,==!==,==
+// const score=200;
+// if(score>100)
+// {
+//     var power="fly";
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);
+
+
+//as usual if,if else,else if statements
+const balance=1000;
+
---
+++ .devcontainer/04_controlflow/switch.js
@@ -0,0 +1,13 @@
+const month="march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+
+    default:
+        console.log("out of switch");
+        break;
+}
